a1fbcd6f8e6922ef7ce7683787ee40898eb3f767
Anderson Martins <anderson@publishpress.com>
2023-04-20 23:37:40 -0300

    Add global constants to know when the lib as loaded and which version

--- lib/include.php
+++ lib/include.php
@@ -16,26 +16,35 @@ if (! function_exists('add_action')) {
     return;
 }
 
-if (! function_exists(__NAMESPACE__ . '\register2Dot0Dot1Dot4')) {
+if (! function_exists(__NAMESPACE__ . '\register2Dot0Dot1Dot5')) {
+    if (! defined('PUBLISHPRESS_PSR_CONTAINER_INCLUDED')) {
+        define('PUBLISHPRESS_PSR_CONTAINER_INCLUDED', __DIR__);
+    }
+        
     if (! class_exists('PublishPress\PsrContainer\Versions')) {
         require_once __DIR__ . '/Versions.php';
 
         add_action('plugins_loaded', [Versions::class, 'initializeLatestVersion'], 1, 0);
     }
 
-    add_action('plugins_loaded', __NAMESPACE__ . '\register2Dot0Dot1Dot4', 1, 0);
+    add_action('plugins_loaded', __NAMESPACE__ . '\register2Dot0Dot1Dot5', 1, 0);
 
-    function register2Dot0Dot1Dot4()
+    function register2Dot0Dot1Dot5()
     {
         if (! interface_exists('PublishPress\Psr\Container\ContainerInterface')) {
             $versions = Versions::getInstance();
-            $versions->register('2.0.1.4', __NAMESPACE__ . '\initialize2Dot0Dot1Dot4');
+            $versions->register('2.0.1.5', __NAMESPACE__ . '\initialize2Dot0Dot1Dot5');
         }
     }
 
-    function initialize2Dot0Dot1Dot4()
+    function initialize2Dot0Dot1Dot5()
     {
         require_once __DIR__ . '/autoload.php';
-        do_action('publishpress_psr_container_2Dot0Dot1Dot4_initialized');
+        
+        if (! defined('PUBLISHPRESS_PSR_CONTAINER_VERSION')) {
+            define('PUBLISHPRESS_PSR_CONTAINER_VERSION', '2.0.1.5');
+        }
+        
+        do_action('publishpress_psr_container_2Dot0Dot1Dot5_initialized');
     }
 }
