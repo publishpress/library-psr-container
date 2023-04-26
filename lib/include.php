f69f504b2feb966c80789e24c2cf237e32c6cab9
Anderson Martins <anderson@publishpress.com>
2023-04-25 22:36:16 -0300

    Bump version to 2.0.1.7

--- lib/include.php
+++ lib/include.php
@@ -16,7 +16,7 @@ if (! function_exists('add_action')) {
     return;
 }
 
-if (! function_exists(__NAMESPACE__ . '\register2Dot0Dot1Dot5')) {
+if (! function_exists(__NAMESPACE__ . '\register2Dot0Dot1Dot7')) {
     if (! defined('PUBLISHPRESS_PSR_CONTAINER_INCLUDED')) {
         define('PUBLISHPRESS_PSR_CONTAINER_INCLUDED', __DIR__);
     }
@@ -29,22 +29,22 @@ if (! function_exists(__NAMESPACE__ . '\register2Dot0Dot1Dot5')) {
 
     add_action('plugins_loaded', __NAMESPACE__ . '\register2Dot0Dot1Dot7', -200, 0);
 
-    function register2Dot0Dot1Dot5()
+    function register2Dot0Dot1Dot7()
     {
         if (! interface_exists('PublishPress\Psr\Container\ContainerInterface')) {
             $versions = Versions::getInstance();
-            $versions->register('2.0.1.5', __NAMESPACE__ . '\initialize2Dot0Dot1Dot5');
+            $versions->register('2.0.1.7', __NAMESPACE__ . '\initialize2Dot0Dot1Dot7');
         }
     }
 
-    function initialize2Dot0Dot1Dot5()
+    function initialize2Dot0Dot1Dot7()
     {
         require_once __DIR__ . '/autoload.php';
         
         if (! defined('PUBLISHPRESS_PSR_CONTAINER_VERSION')) {
-            define('PUBLISHPRESS_PSR_CONTAINER_VERSION', '2.0.1.5');
+            define('PUBLISHPRESS_PSR_CONTAINER_VERSION', '2.0.1.7');
         }
         
-        do_action('publishpress_psr_container_2Dot0Dot1Dot5_initialized');
+        do_action('publishpress_psr_container_2Dot0Dot1Dot7_initialized');
     }
 }
