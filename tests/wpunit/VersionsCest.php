04426619e1777f65b060eafe8a488b5d1e83ba08
Anderson Martins <anderson@publishpress.com>
2023-04-25 22:36:04 -0300

    Adjust action priorities to lower numbers

--- tests/wpunit/VersionsCest.php
+++ tests/wpunit/VersionsCest.php
@@ -21,7 +21,7 @@ class VersionsCest
         $I->assertEquals([
             '2.0.0.1' => 'PublishPress\PsrContainer\initialize2Dot0Dot0Dot1',
             '2.0.0.2' => 'PublishPress\PsrContainer\initialize2Dot0Dot0Dot2',
-            '2.0.1.5' => 'PublishPress\PsrContainer\initialize2Dot0Dot1Dot5',
+            '2.0.1.7' => 'PublishPress\PsrContainer\initialize2Dot0Dot1Dot7',
         ], $registeredVersions);
     }
 
@@ -31,7 +31,7 @@ class VersionsCest
 
         $latestVersion = $versions->latestVersion();
 
-        $I->assertEquals('2.0.1.5', $latestVersion);
+        $I->assertEquals('2.0.1.7', $latestVersion);
     }
 
     public function testLatestVersionCallbackIsTheLastOne(WpunitTester $I)
@@ -40,7 +40,7 @@ class VersionsCest
 
         $latestVersionCallback = $versions->latestVersionCallback();
 
-        $I->assertEquals('PublishPress\PsrContainer\initialize2Dot0Dot1Dot5', $latestVersionCallback);
+        $I->assertEquals('PublishPress\PsrContainer\initialize2Dot0Dot1Dot7', $latestVersionCallback);
     }
 
     public function testInitializeLatestVersion(WpunitTester $I)
@@ -51,7 +51,7 @@ class VersionsCest
 
         $I->assertTrue(interface_exists('PublishPress\Psr\Container\ContainerInterface'));
 
-        $didAction = (bool)did_action('publishpress_psr_container_2Dot0Dot1Dot5_initialized');
+        $didAction = (bool)did_action('publishpress_psr_container_2Dot0Dot1Dot7_initialized');
         $I->assertTrue($didAction);
     }
 }
